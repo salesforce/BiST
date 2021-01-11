af4a6b7d5a1259619620efc06a80e10245a7a1ff
henryhungle <hungle.2018@phdis.smu.edu.sg>
2021-01-11 03:37:58 +0000

    remove temp files

--- scripts/archives/run11.sh
+++
@@ -1,9 +0,0 @@
-device=$1
-is_test=$2
-num_workers=$3
-
-./scripts/run_new5.sh $device 1 $is_test "resnext_101_st" resnext101 50 13000 0.2 128 8 3 summary 1 32 1 query,cap 1 12 \
-    0 seq none dyn 22 0 3 3 0 1 1 1 $num_workers $device
-    
-./scripts/run_new5.sh $device 2 $is_test "resnext_101_st" resnext101 50 13000 0.2 128 8 3 summary 1 32 1 query,cap 1 12 \
-    0 seq none dyn 22 0 3 3 0 1 1 1 $num_workers $device
--- scripts/archives/run3.sh
+++
@@ -1,9 +0,0 @@
-device=$1
-is_test=$2
-num_workers=$3
-
-./scripts/run_new7.sh $device 1 $is_test "resnext_101_st vggish" resnext101+vggish 50 13000 0.2 128 8 3 none 0 32 1 query 1 12 \
-    0 seq none dyn 22 1 3 0 3 1 1 1 1 $num_workers $device
-    
-./scripts/run_new7.sh $device 2 $is_test "resnext_101_st vggish" resnext101+vggish 50 13000 0.2 128 8 3 none 0 32 1 query 1 12 \
-    0 seq none dyn 22 1 3 0 3 1 1 1 1 $num_workers $device
